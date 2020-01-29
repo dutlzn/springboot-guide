e43740f54817db3463de80de711a3665c4e8bda1
dutlzn <2436013662@qq.com>
2020-01-29 23:56:39 +0800

    实现页面跳转、保存用户信息

--- NettySpringboot/front/muxiin/js/app.js
+++ NettySpringboot/front/muxiin/js/app.js
@@ -17,11 +17,32 @@ window.app = {
 		return false;
 	},
 	
-	// 输入对话框
+	/**
+	 * 封装消息提示框 默认mui不支持自定义和居中 用h5+
+	 * @param {Object} msg
+	 * @param {Object} type
+	 */
 	showToast: function(msg,type) {
 		plus.nativeUI.toast(msg, {
 			icon: "image/" + type +".png",
 			verticalAlign: "center"
 		})
+	},
+	
+	/**
+	 * 保存用户的全局对象
+	 * @param {Object} user
+	 */
+	setUserGlobalInfo: function(user) {
+		var userInfoStr = JSON.stringify(user);
+		plus.storage.setItem("userInfo", userInfoStr);
+	},
+	/**
+	 * 获取用户的全局对象
+	 */
+	getUserGlobalInfo: function() {
+		var userInfoStr= plus.storage.getItem("userInfo");
+		return userInfo;
+		
 	}
 }
