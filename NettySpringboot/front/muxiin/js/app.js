3f9a1e4624cac42ea48b912059429cefe11a9f58
dutlzn <2436013662@qq.com>
2020-02-06 16:33:54 +0800

    渲染好友列表

--- NettySpringboot/front/muxiin/js/app.js
+++ NettySpringboot/front/muxiin/js/app.js
@@ -67,6 +67,19 @@ window.app = {
 	setContactList: function(contactList) {
 		var contactListStr = JSON.stringify(contactList);
 		plus.storage.setItem("contactList", contactListStr);
+	},
+	
+	/**
+	 * 获取本地缓存中的联系人列表
+	 */
+	getContactList: function() {
+		var contactListStr = plus.storage.getItem("contactList");
+		
+		if (!this.isNotNull(contactListStr)) {
+			return [];
+		}
+		
+		return JSON.parse(contactListStr);
 	}
 	
 }
